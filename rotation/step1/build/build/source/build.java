b1873d7a2be3229a52a377b9dcaded80db35d693
Joshua Raichur <hello@joshuaraichur.com>
2015-07-21 17:27:02 +0530

    Add: HYPE basics (s1).

--- rotation/step1/build/build/source/build.java
+++ rotation/step1/build/build/source/build.java
@@ -12,3 +12,70 @@ import java.io.InputStream;
 import java.io.OutputStream; 
 import java.io.IOException; 
 
+public class build extends PApplet {
+
+int r1 = 0;
+int r2 = 0;
+int r3 = 0;
+int r4 = 0;
+int r5 = 0;
+
+public void setup() {
+
+  size(600,600);
+  rectMode(CENTER);
+}
+
+public void draw() {
+
+  background(0xffCCCCCC);
+
+  pushMatrix();
+    translate(100,height/2);
+    rotate(radians(r1++));
+    rect(0,0,50,50);
+  popMatrix();
+
+  pushMatrix();
+    translate(200,height/2);
+    rotate(radians(r2++));
+    rect(0,0,50,50);
+  popMatrix();
+
+  pushMatrix();
+    translate(300,height/2);
+    rotate(radians(r3++));
+    rect(0,0,50,50);
+  popMatrix();
+
+  pushMatrix();
+    translate(400,height/2);
+    rotate(radians(r4++));
+    rect(0,0,50,50);
+  popMatrix();
+
+  pushMatrix();
+    translate(500,height/2);
+    rotate(radians(r5++));
+    rect(0,0,50,50);
+  popMatrix();
+
+  line(0,height/2,width,height/2);
+
+  ellipse(100,height/2,5,5);
+  ellipse(200,height/2,5,5);
+  ellipse(300,height/2,5,5);
+  ellipse(400,height/2,5,5);
+  ellipse(500,height/2,5,5);
+
+
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
