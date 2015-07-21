d0266ef9637a247945eaec8ff006ea4a7053f311
Joshua Raichur <hello@joshuaraichur.com>
2015-07-21 16:48:45 +0530

    Add: matrix (s1)

---
+++ matrix/step1/build/build.pde
@@ -0,0 +1,40 @@
+void setup() {
+
+  size(600,600);
+
+  // Remember whats going on
+  pushMatrix();
+    rotate(radians(45));
+    rect(100,100,50,50);
+  // Release to back where it was
+  popMatrix();
+
+  // Remember whats going on
+  pushMatrix();
+    rotate(radians(45));
+    rect(200,100,50,50);
+  // Release to back where it was
+  popMatrix();
+
+  // Remember whats going on
+  pushMatrix();
+    rotate(radians(45));
+    rect(300,100,50,50);
+  // Release to back where it was
+  popMatrix();
+
+  // Remember whats going on
+  pushMatrix();
+    rotate(radians(45));
+    rect(400,100,50,50);
+  // Release to back where it was
+  popMatrix();
+
+  // Remember whats going on
+  pushMatrix();
+    rotate(radians(45));
+    rect(500,100,50,50);
+  // Release to back where it was
+  popMatrix();
+
+}
---
+++ matrix/step1/build/build/source/build.java
@@ -0,0 +1,65 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class build extends PApplet {
+
+public void setup() {
+
+  size(600,600);
+
+  // Remember whats going on
+  pushMatrix();
+    rotate(radians(45));
+    rect(100,100,50,50);
+  // Release to back where it was
+  popMatrix();
+
+  // Remember whats going on
+  pushMatrix();
+    rotate(radians(45));
+    rect(200,100,50,50);
+  // Release to back where it was
+  popMatrix();
+
+  // Remember whats going on
+  pushMatrix();
+    rotate(radians(45));
+    rect(300,100,50,50);
+  // Release to back where it was
+  popMatrix();
+
+  // Remember whats going on
+  pushMatrix();
+    rotate(radians(45));
+    rect(400,100,50,50);
+  // Release to back where it was
+  popMatrix();
+
+  // Remember whats going on
+  pushMatrix();
+    rotate(radians(45));
+    rect(500,100,50,50);
+  // Release to back where it was
+  popMatrix();
+
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
