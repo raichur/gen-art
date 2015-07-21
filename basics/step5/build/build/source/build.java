750504661e2cd4864cf525f2f5dc29f0f184858f
Joshua Raichur <hello@joshuaraichur.com>
2015-07-21 16:42:50 +0530

    Add: step 5

---
+++ basics/step5/build/build/source/build.java
@@ -0,0 +1,60 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class build extends PApplet {
+
+public void setup() {
+
+  size(600,600);
+
+  rotate(radians(10));
+  fill(255);
+  rect(400,100,50,50);
+  noFill();
+  rect(0,0,600,600);
+
+  rotate(radians(10));
+  fill(255);
+  rect(400,100,50,50);
+  noFill();
+  rect(0,0,600,600);
+
+  rotate(radians(10));
+  fill(255);
+  rect(400,100,50,50);
+  noFill();
+  rect(0,0,600,600);
+
+  rotate(radians(10));
+  fill(255);
+  rect(400,100,50,50);
+  noFill();
+  rect(0,0,600,600);
+
+  rotate(radians(10));
+  fill(255);
+  rect(400,100,50,50);
+  noFill();
+  rect(0,0,600,600);
+
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
